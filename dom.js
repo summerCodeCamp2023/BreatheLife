f1346ac883481fa2b0e3acf12aa962669d814003
nursetiti <nosiratalade1@gmail.com>
2023-10-24 12:07:05 +0100

    breathelife

---
+++ dom.js
@@ -0,0 +1,12 @@
+//document.getElementById("heading1").innerHTML='Learning DOM';
+//document.getElementById("para").innerHTML="how can i learn DOM";
+// document.getElementById("FOOD").innerHTML="aishat";
+// document.getElementByTagName("h1")[1].innerHTML="silver";
+//document.getElementByClassName("ANIMALS")[0].innerHTML="chocolate";
+//document.getElementById("img").src= "gif.gif";/document.getElementById("ai").src= "barbie.webp";
+//document.getElementById("AI").src= "cat.jpg";
+//document.getElementById("dte").innerHTML="the date and the time is:"+ " "+Date();
+//document.getElementById("h4").style.color="purple";
+//document.getElementById("p").style.fontFamily="Times New Roman","Times","serif";
+//document.getElementById("p"). style.fontWeightOf="Bolder";
+document.getElementById("h4").style
